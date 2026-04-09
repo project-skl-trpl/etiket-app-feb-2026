5cd5d5ce8d21a8f5be9f34b2e1b594fc8bd42805
Febryan1453 <brayenfebryan@gmail.com>
2026-04-09 17:44:17 +0700

    Auth Part 1

--- lib/data/model/user_model.dart
+++ lib/data/model/user_model.dart
@@ -3,38 +3,40 @@ import 'package:etiket_app/domain/entities/user_entity.dart';
 class UserModel extends UserEntity {
   UserModel({
     required this.token,
-    required this.user,
     required super.id,
     required super.name,
     required super.email,
-    required super.emailVerifiedAt,
-    required super.twoFactorSecret,
-    required super.twoFactorRecoveryCodes,
-    required super.twoFactorConfirmedAt,
     required super.createdAt,
     required super.updatedAt,
+    super.phone,
     required super.role,
   });
 
   final String? token;
-  final UserModel? user;
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
+    final user = json["user"]; // ambil object user
+
     return UserModel(
       token: json["token"],
-      user: json["user"] == null ? null : UserModel.fromJson(json["user"]),
-      id: null,
-      name: '',
-      email: '',
-      emailVerifiedAt: null,
-      twoFactorSecret: null,
-      twoFactorRecoveryCodes: null,
-      twoFactorConfirmedAt: null,
-      createdAt: null,
-      updatedAt: null,
-      role: '',
+      id: user["id"],
+      name: user["name"],
+      email: user["email"],
+      createdAt: DateTime.tryParse(user["created_at"] ?? ""),
+      updatedAt: DateTime.tryParse(user["updated_at"] ?? ""),
+      phone: user["phone"],
+      role: user["role"],
     );
   }
 
-  Map<String, dynamic> toJson() => {"token": token, "user": user?.toJson()};
+  Map<String, dynamic> toJson() => {
+    "token": token,
+    "id": id,
+    "name": name,
+    "email": email,
+    "created_at": createdAt?.toIso8601String(),
+    "updated_at": updatedAt?.toIso8601String(),
+    "phone": phone,
+    "role": role,
+  };
 }
